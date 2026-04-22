c77db38f4268cba1a0cfb4ebe82d18536b085e86
Fede Garcia <84251530+MFedericoGarcia@users.noreply.github.com>
2026-04-22 13:46:01 -0300

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnaPieza
+//
+//  Created by Fede Garcia on 22/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnaPiezaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnaPiezaApp.swift
+//  UnaPieza
+//
+//  Created by Fede Garcia on 22/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct UnaPiezaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
